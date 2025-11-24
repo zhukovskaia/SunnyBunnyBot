d8c16f3eb60d0333a7a428c3e7133988a98682c0
zhukovskaia <Aesvnasasha@outlook.com>
2025-11-24 11:12:39 +0500

    Delete src/InfoCommand.java

--- src/InfoCommand.java
+++
@@ -1,5 +0,0 @@
-class InfoCommand {
-    public static void execute() {
-        System.out.println("Я простой консольный бот на Java");
-        System.out.println("Создан для демонстрации команд");
-    }
