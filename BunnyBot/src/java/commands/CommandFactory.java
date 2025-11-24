3ce81e9e9c9d04e56c7a8749146b8fbb71a7ece4
zhukovskaia <Aesvnasasha@outlook.com>
2025-11-24 14:25:54 +0500

    Create CommandFactory.java

---
+++ BunnyBot/src/java/commands/CommandFactory.java
@@ -0,0 +1,24 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class CommandFactory {
+    private static final Map<String, Command> commands = new HashMap<>();
+
+    static {
+        commands.put("/start", new StartCommand());
+        commands.put("🚀 старт", new StartCommand());
+        commands.put("📝 добавить запись", new AddNoteCommand());
+        commands.put("📖 мои записи", new ShowNotesCommand());
+        commands.put("🗑️ удалить запись", new DeleteNoteCommand());
+        commands.put("📊 количество записей", new NoteCountCommand());
+    }
+
+    public static Command getCommand(String commandText) {
+        return commands.get(commandText.toLowerCase());
+    }
+
+    public static boolean isCommand(String text) {
+        return commands.containsKey(text.toLowerCase()) ||
+                text.startsWith("/") || StringUtils.isNumeric(text);
+    }
+}
