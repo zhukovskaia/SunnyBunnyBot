a7a74f5c2324a82099d0ded4bcd26f7169eea54e
zhukovskaia <Aesvnasasha@outlook.com>
2025-12-01 12:53:42 +0500

    Delete BunnyBot directory

--- BunnyBot/src/java/commands/Command.java
+++
@@ -1,6 +0,0 @@
-import org.telegram.telegrambots.meta.api.objects.Message;
-import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
-
-public interface Command {
-    SendMessage execute(Message message, Object diaryStorage);
-}
